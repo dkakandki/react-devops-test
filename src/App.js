90f7b665f151ac75e5fba1fdeb635fb1360164e3
vitthalj <g.j.vitthal@gmail.com>
2023-02-07 12:02:36 +0100

    updated logo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './devops-logo.svg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.udemy.com/course/devops-bootcamp-for-beginners/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Devops
         </a>
       </header>
     </div>
